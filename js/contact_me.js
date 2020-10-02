d1633d90a04c24ab9f415f6c9244d674cae06cc9
raj1103 <raj.civil1103@gmail.com>
2020-10-02 19:39:08 +0530

    021020201940

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://jddesign.000webhostapp.com/mail/contact_vinod.php",
                 type: "POST",
                 data: {
                     name: name,
